1060ab0bc069e725a3dea9268ada8274b21d49d9
thugitcode <153116032+thugitcode@users.noreply.github.com>
2024-09-22 21:52:56 +0700

    Update edit_author.php

--- admin/author/edit_author.php
+++ admin/author/edit_author.php
@@ -1,55 +1,3 @@
-<?php
-include 'db.php';
-
-if (isset($_GET['id'])) {
-    $id = $_GET['id'];
-    $sql = "SELECT * FROM tacgia WHERE ma_tgia = $id";
-    $result = $conn->query($sql);
-
-    if ($result->num_rows > 0) {
-        $row = $result->fetch_assoc();
-    } else {
-        echo "Không tìm thấy tác giả.";
-        exit;
-    }
-} else {
-    echo "ID không hợp lệ.";
-    exit;
-}
-
-if (isset($_POST['submit'])) {
-    $ten_tgia = $_POST['ten_tgia'];
-    $target_file = $row['hinh_tgia'];
-
-    if (!empty($_FILES["photo"]["name"])) {
-        $target_dir = "uploads/";
-        $target_file = $target_dir . basename($_FILES["photo"]["name"]);
-        $imageFileType = strtolower(pathinfo($target_file, PATHINFO_EXTENSION));
-
-        // Kiểm tra file hình ảnh
-        $check = getimagesize($_FILES["photo"]["tmp_name"]);
-        if ($check !== false) {
-            if (!move_uploaded_file($_FILES["photo"]["tmp_name"], $target_file)) {
-                echo "Đã xảy ra lỗi khi tải lên file.";
-                exit;
-            }
-        } else {
-            echo "File không phải là hình ảnh.";
-            exit;
-        }
-    }
-
-    $sql = "UPDATE tacgia SET ten_tgia='$ten_tgia', hinh_tgia='$target_file' WHERE ma_tgia=$id";
-    if ($conn->query($sql) === TRUE) {
-        echo "Cập nhật tác giả thành công.";
-    } else {
-        echo "Lỗi: " . $sql . "<br>" . $conn->error;
-    }
-}
-?>
-
-
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -66,27 +14,27 @@ if (isset($_POST['submit'])) {
         <nav class="navbar navbar-expand-lg bg-body-tertiary shadow p-3 bg-white rounded">
             <div class="container-fluid">
                 <div class="h3">
-                    <a class="navbar-brand" href="#">Administration</a>
+                    <a class="navbar-brand" href="../home.php">Administration</a>
                 </div>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
                 </button>
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                    <li class="nav-item">
-                        <a class="nav-link" aria-current="page" href="./">Trang chủ</a>
+                <li class="nav-item">
+                        <a class="nav-link" href="../home.php">Trang chủ</a>
                     </li>
                     <li class="nav-item">
                         <a class="nav-link" href="../index.php">Trang ngoài</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="category.php">Thể loại</a>
+                        <a class="nav-link" href="../category/category.php">Thể loại</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link active fw-bold" href="author.php">Tác giả</a>
+                        <a class="nav-link active fw-bold" href="../author/author.php">Tác giả</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="article.php">Bài viết</a>
+                        <a class="nav-link" href="../article/article.php">Bài viết</a>
                     </li>
                 </ul>
                 </div>
@@ -95,30 +43,62 @@ if (isset($_POST['submit'])) {
 
     </header>
     <main class="container mt-5 mb-5">
+
+    <?php
+            include "D:\Study\TLU\Năm ba_Kì 5\Công nghệ web\TH1\btth01_template\btth01\CSE485_2023\db.php";
+            
+            //lấy ra ma_tgia và ten_tgia để hiển thị đúng khi bấm sửa
+            if (isset($_GET['id'])) {
+                $author_id = $_GET['id'];
+            
+                $sql = "SELECT ma_tgia, ten_tgia FROM tacgia WHERE ma_tgia = $author_id";
+                $result = $conn->query($sql);
+            
+                if ($result->num_rows == 1) {
+                    $row = $result->fetch_assoc();
+                    $author_id = $row['ma_tgia'];
+                    $author_name = $row['ten_tgia'];
+                }}
+
+                // bắt đầu sửa
+                // Xử lý khi form được gửi đi
+            if ($_SERVER["REQUEST_METHOD"] == "POST") {
+                // Lấy dữ liệu từ form
+                $catId = $_POST['txtCatId'];
+                $catName = $_POST['txtCatName'];
+
+            // Cập nhật thông tin tgia trong cơ sở dữ liệu
+                $sql = "UPDATE tacgia SET ten_tgia = '$catName' WHERE ma_tgia = $catId";
+
+            if ($conn->query($sql) === TRUE) {
+                header("Location: author.php");
+                } else {
+                echo "Lỗi khi cập nhật thông tin tác giả: " . $conn->error;
+                }
+}
+
+        ?>
+
         <!-- <h3 class="text-center text-uppercase mb-3 text-primary">CẢM NHẬN VỀ BÀI HÁT</h3> -->
         <div class="row">
             <div class="col-sm">
                 <h3 class="text-center text-uppercase fw-bold">Sửa thông tin tác giả</h3>
-                <form action="process_add_author.php" method="post">
+                <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
                 <div class="input-group mt-3 mb-3">
                         <span class="input-group-text" id="lblCatId">Mã tác giả</span>
-                        <input type="text" class="form-control" name="txtCatId" readonly value="1">
+                        <input type="text" class="form-control" name="txtCatId" readonly value="<?php echo $author_id; ?>">
                     </div>
 
                     <div class="input-group mt-3 mb-3">
                         <span class="input-group-text" id="lblCatName">Tên tác giả</span>
-                        <input type="text" class="form-control" name="txtCatName" value = "Nhạc trữ tình">
+                        <input type="text" class="form-control" name="txtCatName" value = "<?php echo $author_name; ?>">
                     </div>
-                    <div class="input-group mt-3 mb-3">
-                        <span class="input-group-text" id="lblPhoto">Ảnh đại diện</span>
-                        <input type="file" class="form-control" name="photo" required>
-                        <img src="img/nhacvietplus.jpg" alt="Ảnh đại diện" class="author-photo ms-3"  style='width: 100px;height:auto;margin: left 10px;'>
-                        
-                    </div>
+
                     <div class="form-group  float-end ">
                         <input type="submit" value="Lưu lại" class="btn btn-success">
-                        <a href="category.php" class="btn btn-warning ">Quay lại</a>
+                        <a href="author.php" class="btn btn-warning ">Quay lại</a>
                     </div>
+
                 </form>
             </div>
         </div>
