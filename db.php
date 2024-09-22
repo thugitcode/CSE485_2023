ced43c529dbd9a7a5d02e2aebe43d8e9797dde88
thugitcode <153116032+thugitcode@users.noreply.github.com>
2024-09-22 21:38:33 +0700

    Update db.php

--- db.php
+++ db.php
@@ -1,19 +1,3 @@
-
-<?php
-$servername = "localhost"; // Tên server của bạn
-$username = "root";        // Username MySQL
-$password = "";            // Mật khẩu MySQL (nếu có)
-$dbname = "BTTH01_CSE485"; // Tên cơ sở dữ liệu
-
-// Tạo kết nối
-$conn = new mysqli($servername, $username, $password, $dbname);
-
-// Kiểm tra kết nối
-if ($conn->connect_error) {
-    die("Kết nối thất bại: " . $conn->connect_error);
-}
-?> 
-=======
 <?php
 $servername = "127.0.0.1"; // Tên server của bạn
 $username = "root";        // Username MySQL
@@ -27,6 +11,4 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
     die("Kết nối thất bại: " . $conn->connect_error);
 }
-else "Kết nối thành công"
 ?> 
-
