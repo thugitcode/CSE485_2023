9e531d1150bfa5fab61f1fd530371adcb9368661
thugitcode <153116032+thugitcode@users.noreply.github.com>
2024-09-22 21:46:24 +0700

    Update delete_article.php

--- admin/article/delete_article.php
+++ admin/article/delete_article.php
@@ -1,5 +1,5 @@
 <?php
-    include 'C:\xampp\htdocs\vd\CSE485_2023\db.php'; // Kết nối đến cơ sở dữ liệu
+    include 'D:\Study\TLU\Năm ba_Kì 5\Công nghệ web\TH1\btth01_template\btth01\CSE485_2023\db.php'; // Kết nối đến cơ sở dữ liệu
 
     // Kiểm tra xem có yêu cầu xóa bài viết không
     if(isset($_GET['ma_bviet'])) {
