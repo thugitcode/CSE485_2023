d16a7c412ca6e433c09bf43936ae6fc7c703ab9f
thugitcode <153116032+thugitcode@users.noreply.github.com>
2024-09-22 21:48:50 +0700

    Update delete_category.php

--- admin/category/delete_category.php
+++ admin/category/delete_category.php
@@ -4,10 +4,10 @@ include 'D:\Study\TLU\Năm ba_Kì 5\Công nghệ web\TH1\btth01_template\btth01\
 
 // Kiểm tra xem có tham số id được truyền qua phương thức GET không
 if(isset($_GET['id'])) {
-    $category_id = $_GET['id'];
+    $author_id = $_GET['id'];
 
     // Chuẩn bị câu lệnh SQL để xóa thể loại từ cơ sở dữ liệu
-    $sql = "DELETE FROM theloai WHERE ma_tloai = $category_id";
+    $sql = "DELETE FROM theloai WHERE ma_tloai = $author_id";
 
     if ($conn->query($sql) === TRUE) {
         // Nếu xóa thành công, chuyển hướng về trang danh sách thể loại
