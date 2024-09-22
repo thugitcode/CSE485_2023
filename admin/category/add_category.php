f1bfad9aa523caf6114c2b0d2fa2b03a549ec388
thugitcode <153116032+thugitcode@users.noreply.github.com>
2024-09-22 21:47:51 +0700

    Update add_category.php

--- admin/category/add_category.php
+++ admin/category/add_category.php
@@ -14,15 +14,15 @@
         <nav class="navbar navbar-expand-lg bg-body-tertiary shadow p-3 bg-white rounded">
             <div class="container-fluid">
                 <div class="h3">
-                    <a class="navbar-brand" href="#">Administration</a>
+                    <a class="navbar-brand" href="../home.php">Administration</a>
                 </div>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
                 </button>
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                    <li class="nav-item">
-                        <a class="nav-link" aria-current="page" href="./">Trang chủ</a>
+                <li class="nav-item">
+                        <a class="nav-link" aria-current="page" href="../home.php">Trang chủ</a>
                     </li>
                     <li class="nav-item">
                         <a class="nav-link" href="../index.php">Trang ngoài</a>
@@ -31,10 +31,10 @@
                         <a class="nav-link active fw-bold" href="category.php">Thể loại</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="author.php">Tác giả</a>
+                        <a class="nav-link" href="../author/author.php">Tác giả</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="article.php">Bài viết</a>
+                        <a class="nav-link" href="../article/article.php">Bài viết</a>
                     </li>
                 </ul>
                 </div>
@@ -60,7 +60,7 @@
                     </div>
                 </form>
                 <?php
-include "D:\Study\TLU\Năm ba_Kì 5\Công nghệ web\TH1\btth01_template\btth01\CSE485_2023\db.php";
+            include "D:\Study\TLU\Năm ba_Kì 5\Công nghệ web\TH1\btth01_template\btth01\CSE485_2023\db.php";
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $catName = $_POST['txtCatName'];
