3e434881964b2b24cd85b66b25cd61653c579a31
Quy Mui <dtquymui8104@gmail.com>
2024-09-22 17:48:19 +0700

    update

--- db.php
+++ db.php
@@ -11,4 +11,5 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
     die("Kết nối thất bại: " . $conn->connect_error);
 }
+else "Kết nối thành công"
 ?> 
